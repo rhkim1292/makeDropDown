a62a5e801be7d15d9275ab2f410493a61807899c
Randy Kim <rhkim1292@gmail.com>
2022-09-25 23:34:26 -0700

    Finish implementing drop down functionality
    Some opinionated styling is added to the drop down element that the
    makeDropDown function is called on.

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ exports.makeDropDown = function(divDropDown) {
     const dropBtn = divDropDown.children[0];
     const divDropDownContent = divDropDown.children[1];
     const ulDropDown = divDropDownContent.children[0];
-    const listItemArray = ulDropDown.children;
 
+    dropBtn.style.width = '100px';
+    dropBtn.style.height = '35px';
+    dropBtn.style.background = 'none';
+    dropBtn.style.color = 'inherit';
+    dropBtn.style.border = '2px solid black';
+    divDropDown.style.width = '100px';
     ulDropDown.style.listStyleType = 'none';
+    ulDropDown.style.margin = '0';
+    ulDropDown.style.padding = '0';
+    ulDropDown.style.textAlign = 'center';
     divDropDownContent.style.display = 'none';
     dropBtn.addEventListener('mouseenter', (e) => {
         console.log('hovering drop down button');
@@ -12,6 +20,12 @@ exports.makeDropDown = function(divDropDown) {
     });
     dropBtn.addEventListener('mouseleave', (e) => {
         console.log('stop hovering drop down button');
+        divDropDownContent.style.display = 'none';
+    });
+    divDropDownContent.addEventListener('mouseenter', (e) => {
+        divDropDownContent.style.display = 'block';
+    });
+    divDropDownContent.addEventListener('mouseleave', (e) => {
         divDropDownContent.style.display = 'none';
     });
 }
